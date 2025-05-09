e8fdbacc5ab7a692c4c2c998b7c53ab675b0b9e9
MicheleCasani <casanimichele@gmail.com>
2025-05-09 14:04:36 +0200

    [UPD] add videogames array

---
+++ vidoegamesArray.js
@@ -0,0 +1,41 @@
+// array di videogiochi
+
+const videogames = [
+    {
+        id: 1,
+        title: "Valorant"
+    },
+    {
+        id: 2,
+        title: "Call of Duty"
+    }, {
+        id: 3,
+        title: "Counter-Strike"
+    }, {
+        id: 4,
+        title: "The Luts of Us"
+    }, {
+        id: 5,
+        title: "red Dead Redemption"
+    }, {
+        id: 6,
+        title: "Battlefield"
+    }, {
+        id: 7,
+        title: "DEATHLOOP"
+    },
+    {
+        id: 8,
+        title: "ELDEN RING"
+    },
+    {
+        id: 9,
+        title: "God Of War"
+    },
+    {
+        id: 10,
+        title: "Little Big Planet"
+    },
+]
+
+export default videogames;
