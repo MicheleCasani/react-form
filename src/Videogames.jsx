dde5b2b0e5b64699977c9066d31913117b6741e6
MicheleCasani <casanimichele@gmail.com>
2025-05-09 15:35:09 +0200

    [UPD]

--- src/Videogames.jsx
+++ src/Videogames.jsx
@@ -1,18 +1,33 @@
 import React, { useState } from 'react';
 import videogames from '../videogamesArray'
 
+const videogamesArray = videogames
 
 
 function Videogames() {
 
-    const [newVideogames, setVideogames] = useState('');
+
+
+    const [newGame, setNewGame] = useState('');
+
+    const [game, setGame] = useState(videogamesArray)
+
+    const addGame = (event) => {
+        event.preventDefault();
+        const trimmedGame = newGame.trim();
+        if (trimmedGame === '') return;
+
+        setGame([...game, { title: trimmedGame }]);
+        setNewGame('');
+    };
+
 
 
     return (
         <>
             <ul className='list-group'>
 
-                {videogames.map((game, index) =>
+                {game.map((game, index) =>
                     <li className='list-group-item' key={index}>
                         {game.title}
                         <button className='btn btn-danger position-absolute top-50 end-0 translate-middle-y '>
@@ -22,10 +37,16 @@ function Videogames() {
                 )}
             </ul>
 
-            <form onSubmit={''}>
+            <form onSubmit={addGame} className='mt-4'>
                 <div className='input-group'>
                     <input
-                        value={newVideogames} />
+                        value={newGame}
+                        onChange={(event) => {
+                            setNewGame(event.target.value)
+                        }}
+                        type='text'
+                        placeholder='Nuovo Gioco'
+                        className='form-control' />
                     <button className='btn btn-primary'>Aggiungi</button>
                 </div>
             </form>
